83e47c30432645c3cdb54c4b7e2911238603e4ed
Raj Shakya <rs7267887611@gmail.com>
2024-12-23 05:15:56 +0530

    Pta nhi kya hoga kal

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { createHtmlPlugin } from 'vite-plugin-html';
 
 export default defineConfig({
   plugins: [
     react(),
-    createHtmlPlugin(),
   ],
   // base: '/revana/', // Ensure this matches your GitHub repository name
 });
